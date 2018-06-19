87c993643c083d201cecbc8351d871840fb485f8
Nicolas Lopez <nicolaslopezj@me.com>
2018-06-19 13:04:57 -0400

    no muere en el home del env

--- web/src/App/Pages/Environment/Home/index.js
+++ web/src/App/Pages/Environment/Home/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from './styles.css'
 import Container from 'orionsoft-parts/lib/components/Container'
-import Form from 'App/components/Environment/Form'
 
 export default class Home extends React.Component {
   static propTypes = {}
@@ -9,9 +8,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className={styles.container}>
-        <Container>
-          <Form formId="rxNeqhqFn3bmikJGW" />
-        </Container>
+        <Container>hello</Container>
       </div>
     )
   }
