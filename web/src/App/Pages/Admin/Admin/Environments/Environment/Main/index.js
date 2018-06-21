1508899b067b707d7292fd2b95db4f64c1dbd598
diegoolavarria <diego.olavarriau@hotmail.com>
2018-06-21 15:36:52 -0400

    remove option for environments

--- web/src/App/Pages/Admin/Admin/Environments/Environment/Main/index.js
+++ web/src/App/Pages/Admin/Admin/Environments/Environment/Main/index.js
@@ -5,8 +5,10 @@ import withGraphQL from 'react-apollo-decorators/lib/withGraphQL'
 import gql from 'graphql-tag'
 import PropTypes from 'prop-types'
 import Button from 'orionsoft-parts/lib/components/Button'
+import MutationButton from 'App/components/MutationButton'
 import {withRouter} from 'react-router'
 import autobind from 'autobind-decorator'
+import withMessage from 'orionsoft-parts/lib/decorators/withMessage'
 
 @withRouter
 @withGraphQL(gql`
@@ -17,8 +19,10 @@ import autobind from 'autobind-decorator'
     }
   }
 `)
+@withMessage
 export default class Main extends React.Component {
   static propTypes = {
+    showMessage: PropTypes.func,
     history: PropTypes.object,
     environment: PropTypes.object
   }
@@ -29,12 +33,33 @@ export default class Main extends React.Component {
     this.props.history.replace('/app')
   }
 
+  @autobind
+  removeEnvironment() {
+    this.props.showMessage('La vista fue eliminada')
+    this.props.history.push(`/admin/environments`)
+  }
+
   render() {
     if (!this.props.environment) return null
     return (
       <div className={styles.container}>
         <Breadcrumbs />
-        <Button onClick={this.goToEnv}>Ir al Ambiente</Button>
+        <div className={styles.buttonContainer}>
+          <div>
+            <Button onClick={this.goToEnv}>Ir al Ambiente</Button>
+          </div>
+          <div>
+            <MutationButton
+              label="Eliminar"
+              title="¿Confirma que desea eliminar esta vista?"
+              confirmText="Confirmar"
+              mutation="removeEnvironment"
+              onSuccess={this.removeEnvironment}
+              params={{environmentId: this.props.environment._id}}
+              danger
+            />
+          </div>
+        </div>
       </div>
     )
   }
