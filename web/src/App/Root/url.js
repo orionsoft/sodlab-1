d932c76c6c583b82ab7f8665c9c98e364f7138ff
Nicolás López <nicolaslopezj@me.com>
2018-07-05 13:38:28 -0400

    force https

--- web/src/App/Root/url.js
+++ web/src/App/Root/url.js
@@ -2,10 +2,14 @@ import getEnv from './getEnv'
 
 const urls = {
   local: `http://${window.location.hostname}:3000`,
-  dev: 'http://api.beta.sodlab.com',
+  dev: 'https://api.beta.sodlab.com',
   prod: 'https://api.sodlab.com'
 }
 
 const env = getEnv()
+
+if (env !== 'local' && window.location.protocol !== 'https:') {
+  window.location.protocol = 'https:'
+}
 
 export default urls[env]
