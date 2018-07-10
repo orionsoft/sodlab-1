bbef89b37fcc3fd0c52afd90ea2659ce31cfe058
uner4s <nicolasfernando@orionsoft.io>
2018-07-10 18:50:37 -0400

    Add field text

---
+++ server/app/helpers/fieldTypes/email/index.js
@@ -0,0 +1,11 @@
+export default {
+  name: 'Email',
+  rootType: String,
+  allowedOperatorsIds: ['exists'],
+  optionsSchema: {},
+  validate(value, options) {
+    if (!/\S+@\S+\.\S+/.test(value)) {
+      return 'invalid email'
+    }
+  }
+}
--- web/src/App/components/fieldTypes/index.js
+++ web/src/App/components/fieldTypes/index.js
@@ -5,8 +5,10 @@ import boolean from './boolean'
 import date from './date'
 import datetime from './datetime'
 import hyperlink from './hyperlink'
+import email from './email'
 
 export default {
+  email,
   hyperlink,
   string,
   checkbox,
