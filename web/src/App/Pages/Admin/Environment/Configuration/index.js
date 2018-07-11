3b5a0fe36b5e76af3141be6b77060afa35f29683
diegoolavarria <diego.olavarriau@hotmail.com>
2018-07-11 15:10:17 -0400

    added profileSchema to Environment config

--- web/src/App/Pages/Admin/Environment/Configuration/index.js
+++ web/src/App/Pages/Admin/Environment/Configuration/index.js
@@ -8,6 +8,7 @@ import withMessage from 'orionsoft-parts/lib/decorators/withMessage'
 import PropTypes from 'prop-types'
 import withGraphQL from 'react-apollo-decorators/lib/withGraphQL'
 import gql from 'graphql-tag'
+import Profiles from './Profiles'
 
 @withMessage
 @withGraphQL(gql`
@@ -23,8 +24,10 @@ import gql from 'graphql-tag'
         _id
       }
       fontName
+      ...adminEnvironmentProfilesUpdateFragment
     }
   }
+  ${Profiles.fragment}
 `)
 export default class Configuration extends React.Component {
   static propTypes = {
@@ -52,6 +55,7 @@ export default class Configuration extends React.Component {
             Guardar
           </Button>
         </Section>
+        <Profiles environment={this.props.environment} />
       </div>
     )
   }
