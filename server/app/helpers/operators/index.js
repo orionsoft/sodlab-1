a3e7eda4d33fd7a492630307c0cad33d21e28f8d
uner4s <nicolasfernando@orionsoft.io>
2018-07-12 15:31:16 -0400

    Add number greater equal filter

--- server/app/helpers/operators/index.js
+++ server/app/helpers/operators/index.js
@@ -1,8 +1,10 @@
 import exists from './exists'
 import stringStartsWith from './stringStartsWith'
 import booleanIs from './booleanIs'
+import numberGreaterEqual from './numberGreaterEqual'
 
 export default {
+  numberGreaterEqual,
   exists,
   stringStartsWith,
   booleanIs
