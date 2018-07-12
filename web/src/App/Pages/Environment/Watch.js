9759f4d04fcf9a675706fd622077f1c58a026629
Nicolás López <nicolaslopezj@me.com>
2018-07-12 15:20:14 -0400

    update all queries when environment changes

--- web/src/App/Pages/Environment/Watch.js
+++ web/src/App/Pages/Environment/Watch.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import withSubscription from 'react-apollo-decorators/lib/withSubscription'
 import gql from 'graphql-tag'
+import autobind from 'autobind-decorator'
 
 @withSubscription(
   gql`
@@ -11,12 +12,18 @@ import gql from 'graphql-tag'
         name
       }
     }
-  `
+  `,
+  'onChange'
 )
 export default class Watch extends React.Component {
   static propTypes = {
     client: PropTypes.object,
     environmentId: PropTypes.string
+  }
+
+  @autobind
+  onChange() {
+    this.props.client.queryManager.reFetchObservableQueries()
   }
 
   render() {
