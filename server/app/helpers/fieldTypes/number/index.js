7ad7fa636cac6effa462ada547a2b3fb4b6e1ad2
uner4s <nicolasfernando@orionsoft.io>
2018-07-12 15:06:29 -0400

    Add number greater than filter.

--- server/app/helpers/fieldTypes/number/index.js
+++ server/app/helpers/fieldTypes/number/index.js
@@ -1,7 +1,7 @@
 export default {
   name: 'Número',
   rootType: Number,
-  allowedOperatorsIds: ['exists'],
+  allowedOperatorsIds: ['exists', 'numberGreaterThan'],
   optionsSchema: null,
   validate(value, options) {}
 }
--- server/app/helpers/operators/index.js
+++ server/app/helpers/operators/index.js
@@ -1,8 +1,10 @@
 import exists from './exists'
 import stringStartsWith from './stringStartsWith'
 import booleanIs from './booleanIs'
+import numberGreaterThan from './numberGreaterThan'
 
 export default {
+  numberGreaterThan,
   exists,
   stringStartsWith,
   booleanIs
