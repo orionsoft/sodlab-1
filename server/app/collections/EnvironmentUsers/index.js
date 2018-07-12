1df0f71057ea00eeacafb324839b8b2b4fae24ac
Bastian Ermann <bastian@ermannb.com>
2018-07-12 17:23:01 -0400

    Change environment_users collection name

--- server/app/collections/EnvironmentUsers/index.js
+++ server/app/collections/EnvironmentUsers/index.js
@@ -2,7 +2,7 @@ import {Collection} from '@orion-js/app'
 import EnvironmentUser from 'app/models/EnvironmentUser'
 
 export default new Collection({
-  name: 'environmentusers',
+  name: 'environment_users',
   model: EnvironmentUser,
   indexes: [{keys: {userId: 1, environmentId: 1}, options: {unique: true}}]
 })
