1c7840aca2508d2e6cc23fa07ecc511fd2c918f0
uner4s <nicolasfernando@orionsoft.io>
2018-07-12 14:46:38 -0400

    Add Equal-Number filter

--- server/app/helpers/fieldTypes/currency/index.js
+++ server/app/helpers/fieldTypes/currency/index.js
@@ -1,7 +1,7 @@
 export default {
   name: 'Moneda',
   rootType: Number,
-  allowedOperatorsIds: ['exists'],
+  allowedOperatorsIds: ['exists', 'equalNumber'],
   optionsSchema: {},
   validate(value, options) {}
 }
--- server/app/helpers/operators/index.js
+++ server/app/helpers/operators/index.js
@@ -1,8 +1,10 @@
 import exists from './exists'
 import stringStartsWith from './stringStartsWith'
 import booleanIs from './booleanIs'
+import equalNumber from './equalNumber'
 
 export default {
+  equalNumber,
   exists,
   stringStartsWith,
   booleanIs
