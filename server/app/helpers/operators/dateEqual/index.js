3db4f8b71112a5bf88a7656427354a6b10185c0b
uner4s <nicolasfernando@orionsoft.io>
2018-07-13 15:16:22 -0400

    Fix filter

--- server/app/helpers/operators/dateEqual/index.js
+++ server/app/helpers/operators/dateEqual/index.js
@@ -2,10 +2,10 @@ export default {
   name: 'Fecha igual a',
   inputType: 'date',
   async resolve(value) {
-    let firstDate = new Date(value)
-    let lastDate = new Date(value)
-    firstDate.setHours(0, 0, 0, 0)
-    lastDate.setHours(23, 59, 59, 999)
-    return {$gte: firstDate, $lte: lastDate}
+    let date = new Date(value)
+    // let lastDate = new Date(value)
+    // firstDate.setHours(0, 0, 0, 0)
+    // lastDate.setHours(23, 59, 59, 999)
+    return {$eq: date}
   }
 }
