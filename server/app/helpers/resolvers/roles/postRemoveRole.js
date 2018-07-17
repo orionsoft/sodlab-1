8f94cf269ce572d60f578af384373c25f548b5a7
diegoolavarria <diego.olavarriau@hotmail.com>
2018-07-17 10:49:26 -0400

    removed unused functions and files

--- server/app/helpers/resolvers/roles/postRemoveRole.js
+++
@@ -1,6 +0,0 @@
-import EnvironmentUsers from 'app/collections/EnvironmentUsers'
-
-export default async function(roleId) {
-  await EnvironmentUsers.update({roles: roleId}, {$pull: {roles: roleId}}, {multi: true})
-  return true
-}
--- server/app/resolvers/Roles/removeRole.js
+++ server/app/resolvers/Roles/removeRole.js
@@ -1,6 +1,5 @@
 import Roles from 'app/collections/Roles'
 import {resolver} from '@orion-js/app'
-// import postRemoveRole from 'app/helpers/resolvers/roles/postRemoveRole'
 
 export default resolver({
   params: {
@@ -12,7 +11,6 @@ export default resolver({
   mutation: true,
   role: 'admin',
   async resolve({roleId}, viewer) {
-    // await postRemoveRole(roleId)
     await Roles.remove(roleId)
     return true
   }
