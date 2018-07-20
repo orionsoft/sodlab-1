9a8a6572d12c6a4e63f78fd8c781c9ade7034601
Bastian Ermann <bastian@ermannb.com>
2018-07-20 15:13:44 -0400

    Remove start.sh from git

--- server/start.sh
+++
@@ -1,7 +0,0 @@
-export AWS_S3_BUCKETNAME="orionjs-testing-filemanager"
-export AWS_S3_ACCESS_KEY_ID="AKIAI7RCBUEWBH4BJILQ"
-export AWS_S3_SECRET_ACCESS_KEY="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"
-export AWS_S3_REGION="us-east-1"
-export MAIL_URL="smtps://AKIAJEKAV4CRIGTIBW7Q:AoGoJFBKqXP6RSMcCuCWN5gqS6k47corHCoWoB5%2FaXl%2F@email-smtp.us-east-1.amazonaws.com"
-export MAIL_FROM="Sodlab <app@sodlab.com>"
-orion start --shell
