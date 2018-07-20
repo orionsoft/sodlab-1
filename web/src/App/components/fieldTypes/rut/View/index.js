b5e0b7f7b05a19f426fbb7373e00a6fc241e37fb
uner4s <nicolasfernando@orionsoft.io>
2018-07-20 15:27:16 -0400

    Fix

--- web/src/App/components/fieldTypes/rut/View/index.js
+++ web/src/App/components/fieldTypes/rut/View/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 export default class View extends React.Component {
   static propTypes = {
-    value: PropTypes.bool
+    value: PropTypes.string
   }
 
   render() {
