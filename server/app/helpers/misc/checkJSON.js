904a37e7e580796ea846e7f217e40649c2663a5c
bermann <bermann@users.noreply.github.com>
2018-07-26 13:48:43 -0400

    Refactor

--- server/app/helpers/misc/checkJSON.js
+++ server/app/helpers/misc/checkJSON.js
@@ -1,6 +1,6 @@
 export default function(array, fields) {
   const missingFieldItem = array.find(item => {
-    return !fields.every(field => Object.keys(item).includes(field) && item[field] !== '')
+    return !fields.every(field => !!item[field])
   })
   return !!missingFieldItem
 }
