46d65b4d361563ae2d3196e7b117cc34b2996ace
Nicolás López <nicolaslopezj@me.com>
2018-07-26 13:54:14 -0400

    Reactividad en los View

--- web/src/App/components/fieldTypes/oneOf/View/Value.js
+++ web/src/App/components/fieldTypes/oneOf/View/Value.js
@@ -2,6 +2,8 @@ import React from 'react'
 import withGraphQL from 'react-apollo-decorators/lib/withGraphQL'
 import gql from 'graphql-tag'
 import PropTypes from 'prop-types'
+import withSubscription from 'react-apollo-decorators/lib/withSubscription'
+import autobind from 'autobind-decorator'
 
 @withGraphQL(
   gql`
@@ -11,9 +13,34 @@ import PropTypes from 'prop-types'
   `,
   {loading: null}
 )
+@withSubscription(
+  gql`
+    subscription($environmentId: ID, $collectionId: ID) {
+      collectionDataChanged(environmentId: $environmentId, collectionId: $collectionId)
+    }
+  `,
+  'onUpdate',
+  {
+    getVariables(props) {
+      const envId = props.options.collectionId.split('_')[0]
+      const vars = {
+        environmentId: envId,
+        collectionId: props.options.collectionId
+      }
+
+      return vars
+    }
+  }
+)
 export default class Value extends React.Component {
   static propTypes = {
-    label: PropTypes.string
+    label: PropTypes.string,
+    refetch: PropTypes.func
+  }
+
+  @autobind
+  onUpdate() {
+    this.props.refetch()
   }
 
   render() {
