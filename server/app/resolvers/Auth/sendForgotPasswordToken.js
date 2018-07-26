7e9e656a493b3e979462e382e7ab0d12cfdf555a
Nicolás López <nicolaslopezj@me.com>
2018-07-26 11:19:49 -0400

    use env url

--- server/app/resolvers/Auth/sendForgotPasswordToken.js
+++ server/app/resolvers/Auth/sendForgotPasswordToken.js
@@ -1,7 +1,7 @@
 import sendEmail from 'app/services/mailer/sendEmail'
 
 export default async function(user, token) {
-  const url = `http://localhost:3010/reset/${token}`
+  const url = `${process.env.CLIENT_URL}/reset/${token}`
   await sendEmail({
     address: await user.email(),
     subject: 'Recuperación de contraseña',
