0a09372d499dc8bb91ea20dcb141031fdea35a1b
Nicolás López <nicolaslopezj@me.com>
2018-07-27 11:43:37 -0400

    autoincrement indicator type

--- server/app/helpers/indicatorTypes/index.js
+++ server/app/helpers/indicatorTypes/index.js
@@ -1,9 +1,11 @@
 import economicIndicator from './economicIndicator'
 import sum from './sum'
 import count from './count'
+import autoIncrement from './autoIncrement'
 
 export default {
   count,
   sum,
-  economicIndicator
+  economicIndicator,
+  autoIncrement
 }
