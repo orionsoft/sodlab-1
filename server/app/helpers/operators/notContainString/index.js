862ba9f72fb578671c31d6eb85793083aae053db
diegoolavarria <diego.olavarriau@hotmail.com>
2018-07-31 16:22:37 -0400

    fix regex 'notContainString' operator

--- server/app/helpers/operators/notContainString/index.js
+++ server/app/helpers/operators/notContainString/index.js
@@ -4,6 +4,6 @@ export default {
   name: 'No contiene',
   inputType: 'string',
   async resolve(value) {
-    return {$regex: new RegExp(`^((?!${escape(value)}).)*$`, $options: '$i'}
+    return {$regex: new RegExp(`^((?!${escape(value)}).)*$`), $options: '$i'}
   }
 }
