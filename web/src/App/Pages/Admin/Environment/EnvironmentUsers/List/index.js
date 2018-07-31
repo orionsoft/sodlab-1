1206399a4adac9aceeb31b0c35e89ecc34b36f9f
uner4s <nicolasfernando@orionsoft.io>
2018-07-31 15:02:03 -0400

    Add schema and roles in environments users paginated

--- web/src/App/Pages/Admin/Environment/EnvironmentUsers/List/index.js
+++ web/src/App/Pages/Admin/Environment/EnvironmentUsers/List/index.js
@@ -5,16 +5,73 @@ import Button from 'orionsoft-parts/lib/components/Button'
 import PaginatedList from 'App/components/Crud/List'
 import PropTypes from 'prop-types'
 import Breadcrumbs from '../../Breadcrumbs'
+import withGraphQL from 'react-apollo-decorators/lib/withGraphQL'
+import gql from 'graphql-tag'
 
+@withGraphQL(gql`
+  query getEnvironmentData($environmentId: ID, $url: String) {
+    environment(environmentId: $environmentId, url: $url) {
+      _id
+      profileFields {
+        name
+      }
+    }
+    roles(environmentId: $environmentId) {
+      items {
+        value: _id
+        label: name
+      }
+    }
+  }
+`)
 @withRouter
 export default class List extends React.Component {
   static propTypes = {
     history: PropTypes.object,
-    match: PropTypes.object
+    match: PropTypes.object,
+    environment: PropTypes.object,
+    roles: PropTypes.object
   }
 
   getFields() {
-    return [{title: 'Email', name: 'email'}]
+    return [
+      {title: 'Email', name: 'email'},
+      {title: 'Schema', name: 'profile', render: ({profile}) => this.renderSchema(profile)},
+      {
+        title: 'Roles',
+        name: 'roles',
+        render: ({roles}) => this.renderRoles(roles)
+      }
+    ]
+  }
+
+  renderSchema(profile) {
+    const {environment} = this.props
+    if (!environment.profileFields || !environment.profileFields.length) return
+    const schema = environment.profileFields.find(field => {
+      return !profile[field.name]
+    })
+    return <div className={styles.schema}>{schema ? 'No' : 'Si'}</div>
+  }
+
+  renderRoles(userRoles) {
+    if (!userRoles || !userRoles.length) return
+    const {roles} = this.props
+    const roleNames = userRoles.map(rol => {
+      return (
+        roles.items.find(role => {
+          return rol === role.value
+        }) || []
+      )
+    })
+    if (!roleNames || !roleNames.length) return
+    return roleNames.map((rol, index) => {
+      return (
+        <div key={index} className={styles.rol}>
+          {rol.label}
+        </div>
+      )
+    })
   }
 
   render() {
