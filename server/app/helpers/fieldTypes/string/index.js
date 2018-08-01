92c90ebcf73b6bfaf60164a1a3d3a7297ba9eaee
uner4s <nicolasfernando@orionsoft.io>
2018-08-01 11:52:13 -0400

    Add all filters to string fieldType and fix checkbox

--- server/app/helpers/fieldTypes/string/index.js
+++ server/app/helpers/fieldTypes/string/index.js
@@ -1,7 +1,15 @@
 export default {
   name: 'Texto',
   rootType: String,
-  allowedOperatorsIds: ['exists', 'stringStartsWith', 'equalString', 'containString'],
+  allowedOperatorsIds: [
+    'exists',
+    'stringStartsWith',
+    'notStringStartsWith',
+    'equalString',
+    'notEqualString',
+    'containString',
+    'notContainString'
+  ],
   optionsSchema: null,
   validate(value, options) {}
 }
