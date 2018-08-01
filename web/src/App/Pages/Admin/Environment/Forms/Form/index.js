29619ccde9e9966b42e1a0b34e6b4a9627a98520
Bastian Ermann <bastian@ermannb.com>
2018-08-01 13:14:24 -0400

    Fix edit form

--- web/src/App/Pages/Admin/Environment/Forms/Form/index.js
+++ web/src/App/Pages/Admin/Environment/Forms/Form/index.js
@@ -14,6 +14,7 @@ import Text from 'orionsoft-parts/lib/components/fields/Text'
 import Select from 'orionsoft-parts/lib/components/fields/Select'
 import {Field, WithValue} from 'simple-react-form'
 import autobind from 'autobind-decorator'
+import Checkbox from 'App/components/fieldTypes/checkbox/Field'
 import Fields from './Fields'
 
 @withGraphQL(gql`
