c1bada5ef8cfff0e091cb29b78d478e4810f35de
Nicolás López Jullian <nicolaslopezj@me.com>
2018-08-02 16:57:54 -0400

    Update index.js

--- server/app/models/Hook/resolvers/index.js
+++ server/app/models/Hook/resolvers/index.js
@@ -1,11 +1,9 @@
 import getOptions from './getOptions'
 import functionType from './functionType'
-import paramsSchema from './paramsSchema'
 import execute from './execute'
 
 export default {
   getOptions,
   functionType,
-  paramsSchema,
   execute
 }
