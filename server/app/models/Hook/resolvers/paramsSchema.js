5cae30c4978717d3b1a3b89dbf219031515a6fb7
Nicolás López Jullian <nicolaslopezj@me.com>
2018-08-02 16:56:02 -0400

    Delete paramsSchema.js

--- server/app/models/Hook/resolvers/paramsSchema.js
+++
@@ -1,8 +0,0 @@
-import {resolver} from '@orion-js/app'
-
-export default resolver({
-  returns: 'blackbox',
-  async resolve(hook, params, viewer) {
-    await hook.execute({params: {email: 'nicolaslopezj@me.com'}})
-  }
-})
