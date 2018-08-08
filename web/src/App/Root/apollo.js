620e322fc26bfbc2fffb27f29ddaf4d92b6081f0
Nicolás López <nicolaslopezj@me.com>
2018-08-08 15:23:14 -0400

    guarda sessión en otro key

--- web/src/App/Root/apollo.js
+++ web/src/App/Root/apollo.js
@@ -1,14 +1,16 @@
 import {createClient} from '@orion-js/graphql-client'
 import url from './url'
 
+const sessionKey = 'sodlabSession'
+
 export default createClient({
   endpointURL: url,
   saveSession(session) {
-    localStorage.setItem('session', JSON.stringify(session, null, 2))
+    localStorage.setItem(sessionKey, JSON.stringify(session, null, 2))
   },
   getSession(session) {
     try {
-      return JSON.parse(localStorage.getItem('session'))
+      return JSON.parse(localStorage.getItem(sessionKey))
     } catch (e) {
       return {}
     }
