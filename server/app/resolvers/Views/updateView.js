4577f549e881312f3e4254a2e7b71a8c16a4f352
diegoolavarria <diego.olavarriau@hotmail.com>
2018-08-13 19:00:01 -0300

    removed map of optional fields

--- server/app/resolvers/Views/updateView.js
+++ server/app/resolvers/Views/updateView.js
@@ -18,12 +18,9 @@ export default resolver({
   mutation: true,
   role: 'admin',
   async resolve({viewId, view: viewData}, viewer) {
-    const fields = ['title']
-    fields.map(field => {
-      if (!viewData.hasOwnProperty(field)) {
-        viewData[field] = null
-      }
-    })
+    if (!viewData.hasOwnProperty('title')) {
+      viewData['title'] = null
+    }
     const view = await Views.findOne(viewId)
     await view.update({$set: viewData})
     return view
