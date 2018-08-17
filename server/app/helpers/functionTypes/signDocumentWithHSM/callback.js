3ed36ad92d015a264f22a1046f13cadf5302cab9
Bastian Ermann <bastian@ermannb.com>
2018-08-17 14:10:01 -0300

    Add logs

--- server/app/helpers/functionTypes/signDocumentWithHSM/callback.js
+++ server/app/helpers/functionTypes/signDocumentWithHSM/callback.js
@@ -7,20 +7,30 @@ route('/callbacks/cloud-hsm', async function({getBody, ...params}) {
   console.log('hsm callback body', body)
   if (!body) return 'invalid request'
   const data = JSON.parse(body)
-  if (!data.request_id) return 'invalid request'
+  console.log(data)
+  if (!data.request_id) {
+    console.log('invalid request')
+    return 'invalid request'
+  }
 
   const request = await Requests.findOne({requestId: data.request_id, status: 'pending'})
-  if (!request) return 'invalid request'
+  if (!request) {
+    console.log('invalid request')
+    return 'invalid request'
+  }
 
   const result = data.results[0]
-  if (!result) return 'no result in results'
+  if (!result) {
+    console.log('no result in results')
+    return 'no result in results'
+  }
 
   if (!result.success) {
     await request.update({$set: {status: 'error', reason: result.error}})
     return 'ok :('
   }
 
-  // await request.update({$set: {status: 'success', url: result.url}})
+  await request.update({$set: {status: 'success', url: result.url}})
 
   const {collectionId, itemId, signedFileKey} = request
 
