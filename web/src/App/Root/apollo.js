8028b163637abe4068abb2d235c7a3f63d267d71
Nicolás López <nicolaslopezj@me.com>
2018-08-22 15:30:30 -0300

    two factor

--- web/src/App/Root/apollo.js
+++ web/src/App/Root/apollo.js
@@ -1,10 +1,12 @@
 import {createClient} from '@orion-js/graphql-client'
 import url from './url'
+import TwoFactorPromptProvider from './TwoFactorPromptProvider'
 
 const sessionKey = 'sodlabSession'
 
 export default createClient({
   endpointURL: url,
+  promptTwoFactorCode: TwoFactorPromptProvider.promptTwoFactor,
   saveSession(session) {
     localStorage.setItem(sessionKey, JSON.stringify(session, null, 2))
   },
