dc92eec02ed72a99dad6ae2d8a2b2ed3975c7105
Javier Gutierrez <javiergs1106@gmail.com>
2018-08-24 19:04:58 -0300

    fix api url

--- web/src/App/components/DocumentEditor/helpers/url.js
+++ web/src/App/components/DocumentEditor/helpers/url.js
@@ -2,4 +2,4 @@ const isProduction = window.location.hostname.includes('sodlab')
 
 const apiUrl = isProduction ? 'https://api.sodlab-document-editor.com' : 'http://localhost:8000'
 
-export default 'http://192.168.2.153:8000'
+export default apiUrl
