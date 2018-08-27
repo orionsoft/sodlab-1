fc83579c627fe420c73f1686be08bd00f2770598
nfmontenegro <nicolas.fmontenegro@gmail.com>
2018-08-27 18:53:55 -0300

    debug off

--- server/app/helpers/indicatorTypes/elapsedTime/index.js
+++ server/app/helpers/indicatorTypes/elapsedTime/index.js
@@ -8,36 +8,16 @@ export default {
     fecha: {
       type: String,
       label: 'Fecha'
-    },
-    operation: {
-      type: String,
-      label: 'Operación',
-      fieldType: 'select',
-      fieldOptions: {
-        options: [
-          {
-            label: 'Timpo transcurrido desde el último documento',
-            value: 'elapsedTimeLastDocument'
-          }
-        ]
-      }
     }
   },
   getRenderType: () => 'text',
-  async getResult({options, collection, fieldName}) {
+  async getResult({collection, fieldName}) {
     const [document] = await collection
       .find()
       .limit(1)
       .sort({$natural: -1})
       .toArray()
 
-    const documents = await collection
-      .find()
-      .toArray()
-
-    //all documents, insert in the field date between elapsed time
-    console.log(documents)
-
-    return `Último documento: ${elapsedTime(document.data.fecha)}`
+    return `Último documento: ${elapsedTime(document.data[fieldName])}`
   }
 }
