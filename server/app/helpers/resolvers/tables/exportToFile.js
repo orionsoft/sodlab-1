bdc1efe135bd2fc8a11dab99c8aab7e73fe6c718
diegoolavarria <diego.olavarriau@hotmail.com>
2018-08-27 17:27:23 -0300

    fixed fields present on table

--- server/app/helpers/resolvers/tables/exportToFile.js
+++ server/app/helpers/resolvers/tables/exportToFile.js
@@ -1,11 +1,19 @@
 import XLSX from 'xlsx'
 
-export default async function(items, footerItems, tableTitle) {
+export const destruct = function(keys, obj) {
+  return keys.reduce((a, c) => ({...a, [c]: obj[c]}), {})
+}
+
+export default async function(items, footerItems, {title, fields}) {
+  const tableFields = fields.map(field => {
+    return field.fieldName
+  })
   const data = await Promise.all(
     items.map(async item => {
+      const dataFields = destruct(tableFields, await item.data)
       return await {
         _id: item._id,
-        ...item.data
+        ...(await dataFields)
       }
     })
   )
@@ -15,11 +23,11 @@ export default async function(items, footerItems, tableTitle) {
   if (footerItems && footerItems.length) {
     XLSX.utils.sheet_add_json(dataSheet, footerItems, {
       skipHeader: true,
-      origin: -1
+      origin: {r: -1, c: 1}
     })
   }
   let book = XLSX.utils.book_new()
-  XLSX.utils.book_append_sheet(book, dataSheet, tableTitle || 'Data')
+  XLSX.utils.book_append_sheet(book, dataSheet, title || 'Data')
 
   return book
 }
