a7cbd771c701df7cabbe26770c45941986482e46
uner4s <nicolasfernando@orionsoft.io>
2018-08-31 16:25:08 -0300

    test

--- server/app/helpers/functionTypes/generatePdf/index.js
+++ server/app/helpers/functionTypes/generatePdf/index.js
@@ -11,6 +11,12 @@ export default {
       fieldType: 'richText',
       label: 'Contenido del pdf'
     },
+    indicatorsIds: {
+      type: [String],
+      label: 'Valores a insertar en el pdf',
+      fieldType: 'indicatorSelect',
+      fieldOptions: {multi: true}
+    },
     name: {
       type: String,
       label: 'Nombre del archivo pdf'
@@ -20,6 +26,10 @@ export default {
       type: String,
       fieldType: 'collectionSelect'
     },
+    itemCollectionId: {
+      type: String,
+      label: 'Id del item de la colección donde guardar el archivo'
+    },
     urlField: {
       type: String,
       label: 'Campo donde insertar el url del pdf',
@@ -27,22 +37,26 @@ export default {
       parentCollection: 'collectionId'
     }
   },
-  async execute({options: {content, name, collectionId, urlField}, itemId}) {
-    const options = {format: 'Letter'}
-
-    let pdfResponse = {}
-    pdfResponse.id = itemId
-    pdfResponse.pdf = await createPdf(content, options)
-
-    const response = await uploadPDF(pdfResponse, 'pdf')
-    const data = {}
-    data[urlField] = `https://s3.amazonaws.com/${response.bucket}/${response.key}`
-    const $set = prependKey(data, 'data')
-
-    const collection = await Collections.findOne(collectionId)
-    const itemCollection = await collection.db()
-    const itemToUpdate = await itemCollection.findOne(itemId)
-
-    await itemToUpdate.update($set)
+  async execute({
+    options: {content, name, collectionId, urlField, itemCollectionId, indicatorsIds},
+    itemId
+  }) {
+    if (!urlField) throw Error('urlField no encontrado')
+    console.log(name, collectionId, urlField, itemCollectionId, indicatorsIds)
+    // const options = {format: 'Letter'}
+    // let pdfResponse = {}
+    // pdfResponse.id = itemId
+    // pdfResponse.pdf = await createPdf(content, options)
+    //
+    // const response = await uploadPDF(pdfResponse, 'pdf')
+    // const data = {}
+    // data[urlField] = `https://s3.amazonaws.com/${response.bucket}/${response.key}`
+    // const $set = prependKey(data, 'data')
+    //
+    // const collection = await Collections.findOne(collectionId)
+    // const itemCollection = await collection.db()
+    // const itemToUpdate = await itemCollection.findOne(itemId)
+    //
+    // await itemToUpdate.update($set)
   }
 }
