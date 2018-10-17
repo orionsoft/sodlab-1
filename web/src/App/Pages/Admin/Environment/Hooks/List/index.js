48242107aa3b31b502b3cc34d37444109deb9710
nfmontenegro <nicolas.fmontenegro@gmail.com>
2018-10-17 13:06:36 -0300

    hook header title

--- web/src/App/Pages/Admin/Environment/Hooks/List/index.js
+++ web/src/App/Pages/Admin/Environment/Hooks/List/index.js
@@ -1,17 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles.css'
+import withGraphQL from 'react-apollo-decorators/lib/withGraphQL'
+import gql from 'graphql-tag'
 import PaginatedList from 'App/components/Crud/List'
 import Breadcrumbs from '../../Breadcrumbs'
 import Button from 'orionsoft-parts/lib/components/Button'
 
+@withGraphQL(gql`
+  query {
+    functionTypes {
+      _id
+      name
+      optionsParams
+    }
+  }
+`)
 export default class List extends React.Component {
   static propTypes = {
     match: PropTypes.object
   }
 
   getFields() {
-    return [{title: 'Título', name: 'name'}]
+    return [
+      {title: 'Nombre', name: 'name'},
+      {
+        title: 'Tipo',
+        name: 'functionTypeId',
+        render: ({functionTypeId}) => this.renderFunctionType(functionTypeId)
+      }
+    ]
+  }
+
+  renderFunctionType(type) {
+    if (!type) return
+    return this.props.functionTypes.filter(({_id}) => _id === type).map(field => field.name)[0]
   }
 
   render() {
