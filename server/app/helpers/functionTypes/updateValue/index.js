8d7948e9c261593c7f753730c55e36915d38398c
Javier Gutierrez <javiergs1106@gmail.com>
2018-10-17 16:21:38 -0300

    Bring back fieldOptions to updateValue hook

--- server/app/helpers/functionTypes/updateValue/index.js
+++ server/app/helpers/functionTypes/updateValue/index.js
@@ -22,21 +22,40 @@ export default {
       label: 'Valor a insertar',
       fieldType: 'collectionFieldSelect'
     },
+    fieldValue: {
+      type: String,
+      label: 'Valor del campo a insertar',
+      fieldType: 'fieldOptions',
+      parentCollection: 'collectionId',
+      parentField: 'value',
+      optional: true
+    },
     useParam: {
       type: Boolean,
       label: 'Usar Parámetro',
-      fieldType: 'checkbox'
+      fieldType: 'checkbox',
+      optional: true
     }
   },
   async execute({options}) {
-    const {collectionId, valueKey, itemId, value, useParam} = options
-    const col = await Collections.findOne(collectionId)
-    const collection = await col.db()
-    const item = await collection.findOne(itemId)
-    if (!item) return
+    const {collectionId, valueKey, itemId, value, fieldValue, useParam} = options
 
-    const newValue = useParam ? value : item.data[value]
+    try {
+      const col = await Collections.findOne(collectionId)
+      const collection = await col.db()
+      const item = await collection.findOne(itemId)
 
-    await item.update({$set: {[`data.${valueKey}`]: newValue}})
+      if (!item) return
+
+      const newValue = useParam ? value : fieldValue ? fieldValue : item.data[value]
+
+      await item.update({$set: {[`data.${valueKey}`]: newValue}})
+    } catch (err) {
+      const newValue = useParam ? value : fieldValue ? fieldValue : `item.data.${value}`
+      console.log(
+        `Error when trying to update the docId: ${itemId} from collection ${collectionId}, from value: ${value} to new value: ${newValue}`,
+        err
+      )
+    }
   }
 }
