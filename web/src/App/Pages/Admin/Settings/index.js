4f8fda05985f4bd37a7b40241426e881d1b933ea
nfmontenegro <nicolas.fmontenegro@gmail.com>
2018-10-26 16:08:18 -0300

    email info

--- web/src/App/Pages/Admin/Settings/index.js
+++ web/src/App/Pages/Admin/Settings/index.js
@@ -6,18 +6,35 @@ import Tabs from 'orionsoft-parts/lib/components/Tabs'
 import PropTypes from 'prop-types'
 import Container from 'orionsoft-parts/lib/components/Container'
 import Security from './Security'
+import withUserId from 'App/helpers/auth/withUserId'
+import withGraphQL from 'react-apollo-decorators/lib/withGraphQL'
+import gql from 'graphql-tag'
 
+@withUserId
+@withGraphQL(gql`
+  query user($userId: ID) {
+    user(userId: $userId) {
+      profile {
+        firstName
+        lastName
+        name
+      }
+      email
+    }
+  }
+`)
 export default class Layout extends React.Component {
   static propTypes = {
     children: PropTypes.node
   }
 
   render() {
+    const {email} = this.props.user
     return (
       <div>
         <Container>
           <div className={styles.title}>
-            <h1>Mi cuenta</h1>
+            <h1>Mi cuenta > {email}</h1>
           </div>
           {/* {title: 'Perfil', path: '/settings'}, */}
           <Tabs items={[{title: 'Seguridad', path: '/settings'}]} />
