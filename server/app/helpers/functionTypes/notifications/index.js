87d2a7451b4d15aee15d70ab33a31b32332326f7
Javier Gutierrez <javiergs1106@gmail.com>
2018-10-26 15:43:04 -0300

    Update notification hook to be used sequentially

--- server/app/helpers/functionTypes/notifications/index.js
+++ server/app/helpers/functionTypes/notifications/index.js
@@ -42,21 +42,47 @@ export default {
   ) {
     let newContent = content
     let newTitle = title
+
     Object.keys(params).forEach(variable => {
       const regexp = new RegExp(`{${escape(variable)}}`, 'g')
       newContent = newContent.replace(regexp, params[variable])
       newTitle = newTitle.replace(regexp, params[variable])
     })
-    const environmentUser = await EnvironmentUsers.findOne({userId, environmentId})
-    await Notifications.insert({
-      title: newTitle,
-      content: newContent,
-      environmentId,
-      path,
-      roles,
-      notifierId: environmentUser._id,
-      createdAt: new Date()
-    })
-    await notificationInserted({environmentId: environmentId}, 'notification')
+
+    let environmentUser
+
+    try {
+      environmentUser = await EnvironmentUsers.findOne({userId, environmentId})
+    } catch (err) {
+      console.log(
+        `Error executing notification hook from env ${environmentId}, with user ${userId}`,
+        err
+      )
+      return {success: false}
+    }
+
+    try {
+      await Notifications.insert({
+        title: newTitle,
+        content: newContent,
+        environmentId,
+        path,
+        roles,
+        notifierId: environmentUser._id,
+        createdAt: new Date()
+      })
+    } catch (err) {
+      console.log(`Error executing notification hook, when trying to create the notification`, err)
+      return {success: false}
+    }
+
+    try {
+      await notificationInserted({environmentId: environmentId}, 'notification')
+    } catch (err) {
+      console.log(`Error executing notification hook, when trying to notify the user`, err)
+      return {success: false}
+    }
+
+    return {success: true}
   }
 }
