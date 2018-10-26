ff90e98db681fd863289e8aaccf4d165461e0916
nfmontenegro <nicolas.fmontenegro@gmail.com>
2018-10-26 14:18:01 -0300

    validate boolean

--- server/app/helpers/validationTypes/index.js
+++ server/app/helpers/validationTypes/index.js
@@ -2,10 +2,12 @@ import numberComparison from './numberComparison'
 import collectionContains from './collectionContains'
 import stringComparison from './stringComparison'
 import verifyType from './verifyType'
+import booleanComparison from './booleanComparison'
 
 export default {
   numberComparison,
   collectionContains,
   stringComparison,
-  verifyType
+  verifyType,
+  booleanComparison
 }
